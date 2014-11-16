c30a87794a80e81e1e906d6d386e94873cfba3b9
Seiei Higa <hanachin@gmail.com>
2014-11-16 21:45:43 +0900

    random :D

--- hi.go
+++ hi.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"time"
 )
 
 func main() {
 	fmt.Printf("Hi\n")
 	fmt.Println("The time is", time.Now())
+	fmt.Println("My favorite number is", rand.Intn(10))
 }
